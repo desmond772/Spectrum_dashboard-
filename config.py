cbade439da49cad44e99a1b620629f4f7157238f
desmond772 <ngwad896@gmail.com>
2025-08-22 15:02:14 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-POCKET_OPTION_SSID_LIVE = os.getenv("POCKET_OPTION_SSID_LIVE", "")
-POCKET_OPTION_SSID_DEMO = os.getenv("POCKET_OPTION_SSID_DEMO", "")
-TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID"29630724")
-TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH"8e12421a95fd722246e0c0b194fd3e0c")
-TELEGRAM_CHANNEL_ONE = os.getenv("TELEGRAM_CHANNEL_ONE"-1002074799242")
-TELEGRAM_CHANNEL_TWO = os.getenv("TELEGRAM_CHANNEL_TWO"-1001940077808")
+python
+import os 
+from dotenv import load_dotenv 
+load_dotenv() 
+POCKET_OPTION_SSID_LIVE = '42["auth",{"session":"R1yh0xAHwNZLSOagoOYKwHhgbVEQbArHjl1FGAqzQPsuJTj_FJ0ijXziAZYKF1Ez34U_zA","isDemo":0}]'
+POCKET_OPTION_SSID_DEMO = '42["auth",{"session":"R1yh0xAHwNZLSOagoOYKwHhgbVEQbArHjl1FGAqzQPsuJTj_FJ0ijXziAZYKF1Ez34U_zA","isDemo":1}]'
+TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID","29630724")
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH","8e12421a95fd722246e0c0b194fd3e0c")
+TELEGRAM_CHANNEL_ONE = os.getenv("TELEGRAM_CHANNEL_ONE","-1002074799242")
+TELEGRAM_CHANNEL_TWO = os.getenv("TELEGRAM_CHANNEL_TWO","-1001940077808")
 DEFAULT_TRADE_AMOUNT = float(os.getenv("DEFAULT_TRADE_AMOUNT", "1.0"))
-TRADE_MODE = os.getenv("TRADE_MODE", "demo")  # "demo" or "live"
+TRADE_MODE = os.getenv("TRADE_MODE", "demo") # "demo" or "live"
 REPORT_PERIODS = ["daily", "weekly", "monthly", "all"]
